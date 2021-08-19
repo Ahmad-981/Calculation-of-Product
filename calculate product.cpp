9efca2b61ceaa46a21c2bf53ea1c3c1e6c9f63aa
AHMAD <89203329+Ahmad-981@users.noreply.github.com>
2021-08-19 22:51:15 +0500

    This program is to calculate product

---
+++ calculate product.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std ;
+int main ()
+{
+	int num1 , num2 , product ;
+	cout << "Enter value of number 1 : " ;
+	cin >> num1 ;
+	cout << "Enter value of number 2 : " ;
+	cin >> num2 ;
+	 product = num1 * num2 ;
+	 cout << "The product of two numbers is equal to : " ;
+	 cout << product ;
+	
+	
+	return 0 ;
+}
